73f3bff5174f870dbe8460f707fd34eae11fdba0
Gabriel Araújo <araujogabrielocn@gmail.com>
2024-09-08 17:53:15 -0300

    feat: adjusting hero section screen sizes

--- src/sections/hero/index.jsx
+++ src/sections/hero/index.jsx
@@ -8,19 +8,31 @@ import CTAButton from '../../components/cta-button';
 function Hero() {
 
   return (
-    <main className="flex flex-col gap-8 pt-4 pb-48"> 
-      <section className="relative p-6 pb-14 mt-4 w-[900px] max-w-[90%] mx-auto rounded-3xl border-solid border-2 border-[var(--primay-light-color)]">
+    <main className="
+      flex flex-col gap-8 pt-4 pb-48
+      lg:flex-row lg:relative lg:w-[90%] lg:mx-auto lg:overflow-hidden lg:pb-32 lg:mb-12
+    "> 
+      <section 
+        className="
+        relative p-6 pb-14 mt-4 max-w-[90%] mx-auto rounded-3xl bg-white border-solid border-2 border-[var(--primay-light-color)]
+        lg:w-[50%] lg:mx-0 lg:p-16 lg:z-20
+        "
+      >
         <h1 className="mb-4">LET'S TURN YOUR VLOGGING PASSION</h1>
         <p className="text-xs">Join Us on Our Blog and Enjoy <span className='hightlight'>High-Quality Content</span> that Inspires  and Provides Solutions</p>
-        <img src={laces} className="absolute -right-5 top-[40%] w-[46px]" />
-        {/* <CTAButton 
-          message="JOIN BE A PARTNER"
-          style={{position: 'absolute', left: '23%', bottom: '-5%'}}
-        /> */}
+        <img src={laces} 
+        className="
+          absolute -right-5 top-[40%] w-[46px] 
+          lg:z-999 lg:top-4
+        "
+        />
         <button className="absolute left-[23%] sm:left-6 -bottom-5 py-2 font-bold w-[200px] rounded-full border-solid border-2 dark-border text-white bg-primary">JOIN BE A PARTNER</button>
       </section>
 
-      <section className="flex mx-auto justify-between w-[90%]">
+      <section className="
+        flex mx-auto justify-between w-[90%]
+        lg:absolute lg:bottom-12 lg:w-[40%]
+      ">
         <div>
           <h1 className="relative text-lg">35.2<span className="absolute top-0 -right-[12px] font-light text-lg primary-text">+</span></h1>
           <span className="block text-center text-[9px] font-bold">REVIEWS</span>
@@ -39,16 +51,39 @@ function Hero() {
         </div>
       </section>
 
-      <section className="flex -z-20 justify-center items-center p-10 pb-14 mt-4 w-[900px] max-w-[90%] mx-auto rounded-3xl border-solid border-2 border-[var(--primay-light-color)]">
-        <div className="relative  w-[160px] md:w-[334px] h-[209px] md:h-[434px] bg-[var(--grey-color)] rounded-full border-solid border-4 dark-border">
-        <img src={leftText} alt="Our Advantages" className="absolute -left-12 -top-10" />
-        <img src={blueArrowDown} className="absolute -right-[70px] top-0" />
-            <ul className="absolute pt-[110px] pb-4 -z-10 -left-[50%] -bottom-[85%] w-[300px] md:w-[453px] flex flex-col justify-center items-center gap-4 rounded-3xl bg-primary">
-              <li className="flex gap-2"><img src={check} /><span className="header-font text-white">TRUSTED</span></li>
-              <li className="flex gap-2"><img src={check} /><span className="header-font text-white">ACTUAL</span></li>
-              <li className="flex gap-2"><img src={check} /><span className="header-font text-white">PROFESSIONAL</span></li>
-              <li className="flex gap-2"><img src={check} /><span className="header-font text-white">ACTUAL</span></li>
-            </ul>
+      <section 
+        className="
+          flex -z-20 justify-center items-center p-10 pb-14 mt-4 w-[900px] max-w-[90%] mx-auto rounded-3xl border-solid border-2 border-[var(--primay-light-color)]
+          lg:p-0 lg:pb-0 lg:mt-0 lg:mx-0 lg:justify-start
+          lg:absolute lg:top-[15%] lg:-right-[35%] lg:pt-12 lg:w-full lg:z-20 lg:h-[250px] lg:border-l-0
+      ">
+        <div 
+        className="
+          relative w-[160px] h-[210px] bg-[var(--grey-color)] rounded-full border-solid border-4 dark-border
+          lg:z-999 lg:absolute lg:left-4 lg:-bottom-20
+        ">
+        <img src={leftText} alt="Our Advantages" 
+          className="
+            absolute -left-12 -top-10
+            lg:rotate-[65deg] lg:left-14 lg:-top-20 lg:w-[160px]
+          "
+        />
+        <img src={blueArrowDown}
+          className="
+            absolute -right-[70px] top-0
+            lg:w-[40px] lg:-right-40 lg:-top-12
+          "
+        />
+        <ul 
+          className="
+            absolute pt-[110px] pb-4 -z-10 -left-[50%] -bottom-[85%] w-[300px] flex flex-col justify-center items-center gap-4 rounded-3xl bg-primary
+            lg:p-4 lg:mt-0 lg:mx-0 lg:left-24 lg:bottom-8 lg:gap-2 lg:rounded-[50px] lg:items-start lg:pl-28
+          ">
+          <li className="flex gap-2"><img src={check} /><span className="header-font text-white lg:text-sm">TRUSTED</span></li>
+          <li className="flex gap-2"><img src={check} /><span className="header-font text-white lg:text-sm">ACTUAL</span></li>
+          <li className="flex gap-2"><img src={check} /><span className="header-font text-white lg:text-sm">PROFESSIONAL</span></li>
+          <li className="flex gap-2"><img src={check} /><span className="header-font text-white lg:text-sm">ACTUAL</span></li>
+        </ul>
         </div>
       </section>
     </main>
--- src/sections/navbar/index.jsx
+++ src/sections/navbar/index.jsx
@@ -10,7 +10,7 @@ function Navbar() {
   };
 
   return (
-    <header className="p-4">
+    <header className="p-4 max-w-[90%]">
       <div className="container mx-auto flex justify-between items-center">
         <div className="flex items-center">
           <img src={logo} alt="Blogger Logo" className="h-5 w-20" />  {/* Usa a logo SVG */}
@@ -23,19 +23,19 @@ function Navbar() {
         </div>
 
         <nav className="hidden md:flex space-x-6">
-          <a href="#home" className="text-gray-700 hover:text-gray-900">HOME</a>
-          <a href="#about" className="text-gray-700 hover:text-gray-900">SERVICE</a>
-          <a href="#about" className="text-gray-700 hover:text-gray-900">PARTNER</a>
-          <a href="#contact" className="text-gray-700 hover:text-gray-900">Contact Us</a>
+          <a href="#home" className="dark-text p-2">HOME</a>
+          <a href="#about" className="dark-text p-2">SERVICE</a>
+          <a href="#about" className="dark-text p-2">PARTNER</a>
+          <a href="#contact" className="text-white rounded-full ml-4 p-2 bg-[var(--default-darker-color)]">Contact Us</a>
         </nav>
       </div>
 
       {isOpen && (
         <nav className="md:hidden mt-4">
-          <a href="#home" className="block py-2 text-gray-700 hover:text-gray-900">HOME</a>
-          <a href="#about" className="block py-2 text-gray-700 hover:text-gray-900">SERVICE</a>
-          <a href="#about" className="block py-2 text-gray-700 hover:text-gray-900">PARTNER</a>
-          <a href="#contact" className="block py-2 text-gray-700 hover:text-gray-900">Contact Us</a>
+          <a href="#home" className="block py-2 dark-text">HOME</a>
+          <a href="#about" className="block py-2 dark-text">SERVICE</a>
+          <a href="#about" className="block py-2 dark-text">PARTNER</a>
+          <a href="#contact" className="block py-2 dark-text">Contact Us</a>
         </nav>
       )}
     </header>
